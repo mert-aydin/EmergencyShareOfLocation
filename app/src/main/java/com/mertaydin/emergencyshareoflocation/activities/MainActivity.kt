90d2a81be5e4bdf60d6e28008215b3fdab97abb1
Mert Aydın <mertaydin97@gmail.com>
2023-05-06 01:11:44 +0300

    Jetpack DataStore adopted.

--- app/src/main/java/com/mertaydin/emergencyshareoflocation/activities/MainActivity.kt
+++ app/src/main/java/com/mertaydin/emergencyshareoflocation/activities/MainActivity.kt
@@ -10,28 +10,49 @@ import android.provider.ContactsContract
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
 import com.google.android.material.chip.Chip
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.mertaydin.emergencyshareoflocation.R
 import com.mertaydin.emergencyshareoflocation.databinding.ActivityMainBinding
 import com.mertaydin.emergencyshareoflocation.models.Contact
-import com.mertaydin.emergencyshareoflocation.utils.Constants.PICKED_CONTACTS
+import com.mertaydin.emergencyshareoflocation.utils.Constants.CONTACTS_DATA_STORE_NAME
+import com.mertaydin.emergencyshareoflocation.utils.Constants.CONTACTS_PREFERENCES_KEY
 import com.mertaydin.emergencyshareoflocation.utils.Constants.READ_CONTACTS_PERMISSION_REQUEST_CODE
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val contacts = arrayListOf<Contact>()
+    private var contacts = arrayListOf<Contact>()
     private val pickContactLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { it?.data?.data?.let { pickContact(it) } }
+    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(CONTACTS_DATA_STORE_NAME)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        readContactsFromPersistentMemory()
-        resetChips()
+        loadContacts()
+    }
+
+    private fun loadContacts() {
+        MainScope().launch {
+            dataStore.data.map {
+                it[stringPreferencesKey(CONTACTS_PREFERENCES_KEY)]
+                        ?: Gson().toJson(arrayListOf<Contact>())
+            }.collect {
+                contacts = Gson().fromJson(it, object : TypeToken<List<Contact>>() {}.type)
+                resetChips()
+            }
+        }
     }
 
     private fun resetChips() {
@@ -56,30 +77,23 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
-    private fun readContactsFromPersistentMemory() {
-        contacts.addAll(Gson().fromJson(getPreferences(Context.MODE_PRIVATE).getString(PICKED_CONTACTS, Gson().toJson(arrayListOf<Contact>())), object : TypeToken<List<Contact>>() {}.type))
-    }
-
-    private fun writeContactsToPersistentMemory() {
-        with(getPreferences(Context.MODE_PRIVATE).edit()) {
-            putString(PICKED_CONTACTS, Gson().toJson(contacts))
-            apply()
-        }
-    }
-
     private fun addContact() {
         pickContactLauncher.launch(Intent(Intent.ACTION_PICK).apply { type = ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE })
     }
 
     private fun pickContact(uri: Uri) {
-        contentResolver?.query(uri, arrayOf(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME_PRIMARY, ContactsContract.CommonDataKinds.Phone.NORMALIZED_NUMBER), null, null, null)?.apply {
+        contentResolver?.query(uri, arrayOf(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME_PRIMARY, ContactsContract.CommonDataKinds.Phone.NORMALIZED_NUMBER, ContactsContract.CommonDataKinds.Phone.NUMBER), null, null, null)?.apply {
             moveToFirst()
-            contacts.add(Contact(getString(0), getString(1)))
+            contacts.add(Contact(getString(0), getString(1) ?: getString(2)))
             close()
         }
 
-        resetChips()
-        writeContactsToPersistentMemory()
+        MainScope().launch {
+            dataStore.edit {
+                it[stringPreferencesKey(CONTACTS_PREFERENCES_KEY)] = Gson().toJson(contacts)
+            }
+            resetChips()
+        }
     }
 
     private fun checkPermission(permission: String, requestCode: Int, ifPermissionAlreadyGranted: () -> Unit) {
--- app/src/main/java/com/mertaydin/emergencyshareoflocation/utils/Constants.kt
+++ app/src/main/java/com/mertaydin/emergencyshareoflocation/utils/Constants.kt
@@ -5,5 +5,6 @@ object Constants {
     const val READ_CONTACTS_PERMISSION_REQUEST_CODE = 1
     const val SEND_SMS_PERMISSION_REQUEST_CODE = 2
     const val LOCATION_PERMISSION_REQUEST_CODE = 3
-    const val PICKED_CONTACTS = "contacts"
+    const val CONTACTS_DATA_STORE_NAME = "contacts"
+    const val CONTACTS_PREFERENCES_KEY = "contacts"
 }
